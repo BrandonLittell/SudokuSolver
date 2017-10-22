5f09c2a9bb9b69ad65ac351e61e4177137fe9e18
Brandon Littell <brandon@brandonlittell.com>
2017-10-21 19:05:17 -0700

    Add Solve and helper

--- src/Board.cs
+++ src/Board.cs
@@ -46,5 +46,31 @@ namespace SudokuSolver
             int SquareId(int i)
                 => (i / 9 / 3 * 3) + (i % 9 / 3);
         }
+
+        public IEnumerable<int> GetPossibleValues(int index)
+        {
+            var possibleValues = Enumerable.Range(1, 9).ToList();
+            var foundValues = GetRow(index).Concat(GetColumn(index).Concat(GetSquare(index))).Distinct();
+            return possibleValues.Except(foundValues);
+        }
+
+        public IEnumerable<int> Solve()
+        {
+            int[] prevBoard;
+            do
+            {
+                prevBoard = (int[])_board.Clone();
+                for (var i = 0; i < 81; i++)
+                {
+                    if (_board[i] != -1) continue;
+                    var possibleValues = GetPossibleValues(i);
+                    if (possibleValues.Count() == 1)
+                        _board[i] = possibleValues.First();
+                }
+            }
+            while(!_board.SequenceEqual(prevBoard));
+
+            return _board;
+        }
     }
 }
