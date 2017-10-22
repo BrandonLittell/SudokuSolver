fdaa3f59b2e43c335a9a6323d342e5d23fbfa555
Brandon Littell <brandon@brandonlittell.com>
2017-10-22 08:41:07 -0700

    Reconfigure project structure, add tests project

---
+++ src/SudokuSolver.Tests/Board_ArrayMathTests.cs
@@ -0,0 +1,14 @@
+using System;
+using Xunit;
+
+namespace SudokuSolver.Tests
+{
+    public class Board_ArrayMathTests
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
